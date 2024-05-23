bc1895ac87f3777fca078317b2838c327f2415c8
MantisLord <MantisLord@users.noreply.github.com>
2024-05-22 20:35:37 -0400

    finish porting player controller

--- script/audio_manager.gd
+++ script/audio_manager.gd
@@ -2,3 +2,5 @@ extends Node
 
 @onready var button_down_sfx = $ButtonDownAudioStreamPlayer
 @onready var button_up_sfx = $ButtonUpAudioStreamPlayer
+@onready var jump_sfx = $JumpAudioStreamPlayer
+@onready var footsteps_sfx = $FootstepsAudioStreamPlayer
--- script/player.gd
+++ script/player.gd
@@ -1,12 +1,28 @@
 extends CharacterBody3D
 
 const SPEED_RUN = 5.0
+const SPEED_SPRINT = 8.0
+const SPEED_CROUCH = 3.0
+const SPEED_AIR = 0.5
+const SPEED_GROUND_ACCEL = 10.0
+const SPEED_GROUND_DECCEL = 7.0
+const SPEED_HEAD = 10.0
+const SPEED_CAMERA = 8.0
 const JUMP_VELOCITY = 4.5
 const MIN_ANGLE_VIEW = -40
 const MAX_ANGLE_VIEW = 60
+const CROUCH_DEPTH = -0.5
+const HEAD_BOB_FREQ = 2.4
+const HEAD_BOB_AMP = 0.08
+const FOV_BASE = 75.0
+const FOV_CHANGE = 1.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var play_footsteps_sfx = false
+var speed = 0.0
+var head_bob_time = 0.0
 
 func _process(delta):
 	if Input.is_action_just_pressed("menu"):
@@ -26,19 +42,65 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		play_footsteps_sfx = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		AudioManager.jump_sfx.play()
+		play_footsteps_sfx = false
 
-	# Get the input direction and handle the movement/deceleration.
+	# Get the input direction.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = ($Head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED_RUN
-		velocity.z = direction.z * SPEED_RUN
+
+	# Handle move speed.
+	if Input.is_action_pressed("crouch"):
+		$Head.position.y = lerp($Head.position.y, CROUCH_DEPTH, delta * SPEED_HEAD)
+		speed = SPEED_CROUCH
+		AudioManager.footsteps_sfx.pitch_scale = 0.6
+		%CrouchCollisionShape3D.disabled = false;
+		%StandCollisionShape3D.disabled = true;
+	elif !%HeadCollisionRayCast3D.is_colliding():
+		%CrouchCollisionShape3D.disabled = true;
+		%StandCollisionShape3D.disabled = false;
+		$Head.position.y = lerp($Head.position.y, 0.5, delta * SPEED_HEAD)
+		if Input.is_action_pressed("sprint"):
+			speed = SPEED_SPRINT
+			AudioManager.footsteps_sfx.pitch_scale = 1.6
+		else:
+			speed = SPEED_RUN
+			AudioManager.footsteps_sfx.pitch_scale = 1.0
+
+	# Handle the movement/deceleration.
+	if is_on_floor():
+		if direction:
+			play_footsteps_sfx = true
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * SPEED_GROUND_ACCEL)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * SPEED_GROUND_ACCEL)
+		else:
+			play_footsteps_sfx = false
+			velocity.x = lerp(velocity.x, 0.0, delta * SPEED_GROUND_DECCEL)
+			velocity.z = lerp(velocity.z, 0.0, delta * SPEED_GROUND_DECCEL)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED_RUN)
-		velocity.z = move_toward(velocity.z, 0, SPEED_RUN)
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * SPEED_AIR)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * SPEED_AIR)
+		
+	# Handle head bob.
+	head_bob_time += delta * velocity.length() * 1 if is_on_floor() else 0
+	var pos = Vector3.ZERO
+	pos.y = sin(head_bob_time * HEAD_BOB_FREQ) * HEAD_BOB_AMP
+	pos.x = cos(head_bob_time * HEAD_BOB_FREQ / 2) * HEAD_BOB_AMP
+	%Cam.transform.origin = pos
+	
+	if !play_footsteps_sfx:
+		AudioManager.footsteps_sfx.stop()
+	elif !AudioManager.footsteps_sfx.playing:
+		AudioManager.footsteps_sfx.play(randi_range(0, 50))
+	
+	# FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPEED_SPRINT * 2.0)
+	var target_fov = FOV_BASE + FOV_CHANGE * velocity_clamped
+	%Cam.fov = lerp(%Cam.fov, target_fov, delta * SPEED_CAMERA)
 
 	move_and_slide()
